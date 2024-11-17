555251c1d55f11e39e4acf22fe3a0d293db5de57
Alexis2707 <alexis.lambert2707@gmail.com>
2024-11-17 14:58:10 -0500

    Fix ubuntu network

--- mongodb-3-nodes/init_mongo_cluster.sh
+++ mongodb-3-nodes/init_mongo_cluster.sh
@@ -6,7 +6,8 @@ docker-compose down
 
 docker-compose up --build -d
 if [[ $? -eq 0 ]]; then
-    mongosh --host mongo-node1.localhost --port 27017 --eval "rs.status()"
+    sleep 1
+    docker exec mongo1 mongosh --eval "rs.status()"
     echo "Done"
 else
     echo "Failed to launch the cluster\nExiting"
