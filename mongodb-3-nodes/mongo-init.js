0e30527408a42cc805a2fba7a3bd81d8dec5cadb
Baptiste Rosselet <baptiste.rosselet@protonmail.com>
2024-11-17 15:17:30 -0500

    init-mongo.js: adding priority to replicates + adding a product element at db startup

--- mongodb-3-nodes/mongo-init.js
+++ mongodb-3-nodes/mongo-init.js
@@ -1,8 +1,16 @@
 rs.initiate({
     _id : "rs0",
      members : [
-         {_id : 0, host : "mongo1:27017"},
-         {_id : 1, host : "mongo2:27017"},
-         {_id : 2, host : "mongo3:27017"},
+         {_id : 0, host : "mongo1:27017", priority: 1},
+         {_id : 1, host : "mongo2:27017", priority: 0.5},
+         {_id : 2, host : "mongo3:27017", priority: 0.5},
      ]
 });
+
+db.products.insert(
+   [
+     { _id: 11, item: "pencil", qty: 50, type: "no.2" },
+     { item: "pen", qty: 20 },
+     { item: "eraser", qty: 25 }
+   ]
+)
