226a4a33906e9604e3913ebe764db344ca23555b
Alexis2707 <alexis.lambert2707@gmail.com>
2024-11-19 18:21:05 -0500

    Added Redis test line to run_result

--- run_results_ycsb.sh
+++ run_results_ycsb.sh
@@ -16,5 +16,8 @@ do
         bash ycsb-0.17.0/bin/ycsb.sh load mongodb -s -P ycsb-0.17.0/workloads/$workload -p operationcount=50000 -p recordcount=50000 >> outputLoad-$workload.txt; 
         bash ycsb-0.17.0/bin/ycsb.sh run mongodb -s -P ycsb-0.17.0/workloads/$workload -p operationcount=50000 -p recordcount=50000 >> outputRun-$workload.txt; 
         mongosh --eval "use ycsb" --eval  "db.dropDatabase()"
+	# ./bin/ycsb load redis -P workloads/workloada -p "redis.host=172.18.0.2" -p "redis.port=7000" -p "operationcount=50000" -p "recordcount=50000" >> outputRun-$wordload.txt;
+	# ./bin/ycsb run redis -P workloads/workloada -p "redis.host=172.18.0.2" -p "redis.port=7000" -p "operationcount=50000" -p "recordcount=50000" >> outputRun-$wordload.txt;
+
     done
 done
