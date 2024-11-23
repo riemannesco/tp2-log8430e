f3202513a64f8e4d331f1a00aea01d3c31581f94
Alexis2707 <alexis.lambert2707@gmail.com>
2024-11-23 23:24:44 +0100

    Add test result

--- run_results_ycsb.sh
+++ run_results_ycsb.sh
@@ -13,11 +13,10 @@ do
     for i in {1..10}
     do
         echo "Test #$i" 
-        bash ycsb-0.17.0/bin/ycsb.sh load mongodb -s -P ycsb-0.17.0/workloads/$workload -p operationcount=50000 -p recordcount=50000 >> outputLoad-$workload.txt; 
-        bash ycsb-0.17.0/bin/ycsb.sh run mongodb -s -P ycsb-0.17.0/workloads/$workload -p operationcount=50000 -p recordcount=50000 >> outputRun-$workload.txt; 
-        mongosh --eval "use ycsb" --eval  "db.dropDatabase()"
-	# ./bin/ycsb load redis -P workloads/workloada -p "redis.host=172.18.0.2" -p "redis.port=7000" -p "operationcount=50000" -p "recordcount=50000" >> outputRun-$wordload.txt;
-	# ./bin/ycsb run redis -P workloads/workloada -p "redis.host=172.18.0.2" -p "redis.port=7000" -p "operationcount=50000" -p "recordcount=50000" >> outputRun-$wordload.txt;
-
+        # bash ycsb-0.17.0/bin/ycsb.sh load mongodb -s -P ycsb-0.17.0/workloads/$workload -p operationcount=50000 -p recordcount=50000 >> outputLoad-$workload.txt; 
+        # bash ycsb-0.17.0/bin/ycsb.sh run mongodb -s -P ycsb-0.17.0/workloads/$workload -p operationcount=50000 -p recordcount=50000 >> outputRun-$workload.txt; 
+        # docker exec -it mongo1 mongosh --eval "use ycsb" --eval  "db.dropDatabase()"
+        bash ycsb-0.17.0/bin/ycsb.sh load redis -s -P ycsb-0.17.0/workloads/$workload -p "redis.host=172.18.0.2" -p "redis.port=6379" -p operationcount=50000 -p recordcount=50000 >> outputLoad-$workload.txt; 
+        bash ycsb-0.17.0/bin/ycsb.sh run redis -s -P ycsb-0.17.0/workloads/$workload -p "redis.host=172.18.0.2" -p "redis.port=6379" -p operationcount=50000 -p recordcount=50000 >> outputRun-$workload.txt; 
     done
 done
