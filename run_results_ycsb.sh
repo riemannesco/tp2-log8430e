9889dcaeb538eae76166dab5555f0a4b09940f33
Alexis2707 <alexis.lambert2707@gmail.com>
2024-11-25 06:24:54 +0100

    Final workload result

--- run_results_ycsb.sh
+++ run_results_ycsb.sh
@@ -8,15 +8,16 @@
 
 # rm outputLoad.txt
 # rm outputRun.txt
-for workload in "workloada" "workloadc" "tsworkloada"
+# for workload in "workloada" "workloadc" "tsworkloada"
+for workload in "workloadg"
 do
     for i in {1..10}
     do
         echo "Test #$i" 
-        bash ycsb-0.17.0/bin/ycsb.sh load mongodb -s -P ycsb-0.17.0/workloads/$workload -p operationcount=50000 -p recordcount=50000 >> outputLoad-$workload.txt; 
-        bash ycsb-0.17.0/bin/ycsb.sh run mongodb -s -P ycsb-0.17.0/workloads/$workload -p operationcount=50000 -p recordcount=50000 >> outputRun-$workload.txt; 
-        docker exec -it mongo1 mongosh --eval "use ycsb" --eval  "db.dropDatabase()"
-        # bash ycsb-0.17.0/bin/ycsb.sh load redis -s -P ycsb-0.17.0/workloads/$workload -p "redis.host=172.18.0.2" -p "redis.port=6379" -p operationcount=50000 -p recordcount=50000 >> outputLoad-$workload.txt; 
-        # bash ycsb-0.17.0/bin/ycsb.sh run redis -s -P ycsb-0.17.0/workloads/$workload -p "redis.host=172.18.0.2" -p "redis.port=6379" -p operationcount=50000 -p recordcount=50000 >> outputRun-$workload.txt; 
+        # bash ycsb-0.17.0/bin/ycsb.sh load mongodb -s -P ycsb-0.17.0/workloads/$workload -p operationcount=50000 -p recordcount=50000 >> outputLoad-$workload.txt; 
+        # bash ycsb-0.17.0/bin/ycsb.sh run mongodb -s -P ycsb-0.17.0/workloads/$workload -p operationcount=50000 -p recordcount=50000 >> outputRun-$workload.txt; 
+        # docker exec -it mongo1 mongosh --eval "use ycsb" --eval  "db.dropDatabase()"
+        bash ycsb-0.17.0/bin/ycsb.sh load redis -s -P ycsb-0.17.0/workloads/$workload -p "redis.host=172.18.0.2" -p "redis.port=6379" -p operationcount=50000 -p recordcount=50000 >> outputLoad-$workload.txt; 
+        bash ycsb-0.17.0/bin/ycsb.sh run redis -s -P ycsb-0.17.0/workloads/$workload -p "redis.host=172.18.0.2" -p "redis.port=6379" -p operationcount=50000 -p recordcount=50000 >> outputRun-$workload.txt; 
     done
 done
